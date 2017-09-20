21b140ccf876fa552e692023b73cbb8ce6b974ce
Prashant <prashant.patel77@gmail.com>
2017-09-20 23:18:25 +1000

    Added binding with view and ui model

--- app/src/main/java/com/example/fetchgitsample/MainActivity.kt
+++ app/src/main/java/com/example/fetchgitsample/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.fetchgitsample
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.example.fetchgitsample.databinding.ActivityMainBinding
+import com.example.fetchgitsample.models.Repository
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var repository = Repository("This is Sample Android App", "Prashant Patel", 1000, true )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.repository = repository
+        binding.executePendingBindings()
+
+//        binding.apply {
+//            repositoryName.text = "This is Sample Android App"
+//            repositoryOwner.text = "Prashant Patel"
+//            numberOfStars.text = "10 stars"
+//        }
     }
 }
