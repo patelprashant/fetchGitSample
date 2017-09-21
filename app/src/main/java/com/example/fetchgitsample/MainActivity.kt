01e66732e36a7635ab5e1d76cc8fe9595603bc34
Prashant <prashant.patel77@gmail.com>
2017-09-21 21:48:06 +1000

    Change repo data after 200ms with Handler

--- app/src/main/java/com/example/fetchgitsample/MainActivity.kt
+++ app/src/main/java/com/example/fetchgitsample/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.fetchgitsample
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import com.example.fetchgitsample.databinding.ActivityMainBinding
 import com.example.fetchgitsample.models.Repository
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var repository = Repository("This is Sample Android App", "Prashant Patel", 1000, true )
+    private var repository = Repository("This is Sample Android App", "Prashant Patel", 1000, true)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +17,11 @@ class MainActivity : AppCompatActivity() {
         binding.repository = repository
         binding.executePendingBindings()
 
+        Handler().postDelayed({
+            repository.repositoryName = "This is New Sample Android App"
+            binding.repository = repository
+            binding.executePendingBindings()
+        }, 2000)
 //        binding.apply {
 //            repositoryName.text = "This is Sample Android App"
 //            repositoryOwner.text = "Prashant Patel"
